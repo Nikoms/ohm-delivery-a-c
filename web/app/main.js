a1d60a7a870975f3dcd540f26a6c2e312e8f7fce
John Doe <john@doe.org>
2021-10-15 21:31:56 +0200

    Allow displaying resistance found

--- web/app/main.js
+++ web/app/main.js
@@ -3,10 +3,10 @@ angular
     .controller("tracking", function($scope, $http) {
         $scope.sendData = function() {
             $http.get(`http://localhost:3000/ohms/${this.trackingId}`)
-            .then((error) => {
-                this.errorMessage = 'Oops, this website is under construction, please come back later.';
-            }, (result) => {
-                this.errorMessage = '';
+            .then((result) => {
+                this.resistance = result.data;
+            }, (error) => {
+                this.errorMessage = 'Cannot fetch the data from server';
             });
         };
     });
